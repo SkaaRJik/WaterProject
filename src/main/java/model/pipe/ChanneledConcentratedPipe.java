bb4de1bb81f93520c6014ed757c4bd71f33b4549
Filippov Victor <filippowvitia@yandex.com>
2018-03-08 16:26:09 +0800

    ChanneledConcentratedPipe realised

--- src/main/java/model/pipe/ChanneledConcentratedPipe.java
+++ src/main/java/model/pipe/ChanneledConcentratedPipe.java
@@ -2,12 +2,15 @@ package model.pipe;
 
 import model.pipe.mode.Mode;
 import model.river.River;
+import utils.RiverMath;
 
 /**
  * Русловой рассеивающий выпуск
  */
 public class ChanneledConcentratedPipe extends BasePipe {
 
+    int rowLocation;
+    int columnLocation;
     double length;
 
     public ChanneledConcentratedPipe(double x, boolean coast, Mode mode, double length, double concentartion, double wastewaterConsumption){
@@ -22,16 +25,22 @@ public class ChanneledConcentratedPipe extends BasePipe {
 
     @Override
     public void mergeWaste(double[][] river) {
-
+        river[rowLocation][columnLocation] = this.initialDilution;
     }
 
     @Override
     public void calculateInitialDilution(River riverInfo, int rows) {
-        this.initialDilution
+        double Ccp = RiverMath.averageImpurityConcentration(this.concentration,
+                riverInfo.backgroundConcentration, this.wastewaterConsumption, riverInfo.riverWidth,
+                riverInfo.riverDepth,riverInfo.flowSpeed);
+        this.initialDilution = Ccp * rows - riverInfo.backgroundConcentration * rows - 1;
     }
 
     @Override
     public void putPipeOnRiver(double cellSizeX, double cellSizeY, int rows, int columns) {
-
+        this.columnLocation = (int)(this.x / cellSizeX);
+         /* coast  false - правый берег; true - левый берег;  */
+        if(this.coast == false) this.rowLocation = (int)(this.length / cellSizeY);
+        else this.rowLocation = rows - (int)(this.length / cellSizeY) -1;
     }
 }
